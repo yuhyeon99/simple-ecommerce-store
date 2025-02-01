05e4458983536303dd4bb274e3be5634ad67a34c
yuhyeon99 <jujini31@gmail.com>
2025-02-01 20:16:29 +0900

    refactor: home 폴더 내부에서 외부에서 사용할 HomePage 컴포넌트를 제외한 나머지 컴포넌트들은 내보내지 않도록 수정

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,2 +1,1 @@
 export { default as HomePage } from "./HomePage";
-export * from "./components";  // components 폴더의 index.ts도 불러옴
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,1 @@
-export * from "./home"; // home/index.ts를 가져옴
+export { HomePage } from "./home"; // home/index.ts를 가져옴
